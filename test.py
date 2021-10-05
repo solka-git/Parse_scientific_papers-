1af45d7047c1aee87f8c381850b076d4bafb6893
Olha Knysh <oliaknish17@gmail.com>
2021-10-05 17:05:30 +0300

    modified/refactoring test.py for parsing 3 keywords

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import pandas as pd
 
 # HOST = 'https://oatd.org/'
 KEY_WORD = 'quantum+cryptography'
@@ -9,7 +10,7 @@ KEY_WORD_3 = 'automated+and+adaptive+networks'
 URL = f'https://oatd.org/oatd/search?q={KEY_WORD}&form=basic'
 URL_2 = f'https://oatd.org/oatd/search?q={KEY_WORD_2}&form=basic'
 # URL_3 = f'https://oatd.org/oatd/search?q={KEY_WORD_3}&form=basic'
-URL_3 = 'https://oatd.org/oatd/search?q=automated+and+adaptive+networks&form=basic&start=31'
+URL_3 = f'https://oatd.org/oatd/search?q={KEY_WORD_3}&form=basic&start=31'
 
 
 def get_html(url):
@@ -19,21 +20,26 @@ def get_html(url):
     return response.text
 
 
-def key_word_processing(file_name, key, word):
+def item_processing(item, word):
+    item_ = item.replace('Subjects/Keywords:', '').replace('.', '').replace('/', '').strip().lower()
+    if item_ != word and not item_.isdigit():
+        return item_
+    return None
 
+
+def key_word_processing(file_name, key, word):
     new_list = get_file_data(file_name)
     if len(key) > 18:
         if key[0:18] == 'Subjects/Keywords:':
             ll = key.split(';')
             for i in ll:
-                item = i.replace('Subjects/Keywords:', '').replace('.', '').replace('/', '').strip().lower()
-                if item != word and not item.isdigit():
+                item = item_processing(i, word)
+                if item is not None:
                     new_list.append(item)
     else:
-        item = key.replace(';', '').replace('.', '').replace('/', '').lower()
-        if item != word and not item.isdigit():
+        item = item_processing(key, word)
+        if item is not None:
             new_list.append(item)
-
     save_to_file(new_list, file_name)
 
 
@@ -59,10 +65,43 @@ def parse_page_in_json(url, filename, keyword):
         key_word_processing(filename, key_word.text, keyword)
 
 
+def save_to_xlsx_pandas(list_):
+    df = pd.DataFrame(list_)
+    writer = pd.ExcelWriter("technologies.xlsx", engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='x1')
+    writer.save()
+
+
+def list_sort(list_):
+    """
+    :param list_: list from .json
+    :return: dict('technology': quantity)
+    """
+    dict_ = {i: list_.count(i) for i in list_}
+    sorted_dict = dict(sorted(dict_.items(), key=lambda kv: kv[1], reverse=True))
+    return sorted_dict
+
+
+def json_to_list(file):
+    list_ = get_file_data(file)
+    return list(list_sort(list_))
+
+
 def main():
     parse_page_in_json(URL, "quantum_crypt.json", 'quantum cryptography')
     parse_page_in_json(URL_2, "iot_security.json", 'iot security')
     parse_page_in_json(URL_3, "auto_adapt_net.json", 'automated and adaptive networks')
+
+    sort_list1 = json_to_list('quantum_crypt.json')
+    sort_list2 = json_to_list('iot_security.json')
+    sort_list3 = json_to_list('auto_adapt_net.json')
+
+    dict_ = {
+        'Quantum cryptography': sort_list1[:50],
+        'IoT security': sort_list2[:50],
+        'Automated and adaptive networks': sort_list3[:50]
+    }
+    save_to_xlsx_pandas(dict_)
 
 
 if __name__ == '__main__':
